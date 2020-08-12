3a53d7b9e837274cdbf1f35386c9a22bc0d1011b
LuthyMendel <giom.01@hotmail.com>
2020-08-12 00:27:06 -0300

    paginação e Ordenação da Tabela

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,9 +1,11 @@
 package com.algaworks.brewer.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.Origem;
 import com.algaworks.brewer.model.Sabor;
@@ -71,14 +74,17 @@ public class CervejasController {
 	}
 	
 	@GetMapping
-	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result, @PageableDefault(size=2) Pageable pageable) { // o Spring crie esse objeto
+	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result
+			, @PageableDefault(size=2) Pageable pageable, HttpServletRequest httpServletRequest) { // o Spring crie esse objeto
 		
 		ModelAndView mv = new ModelAndView("cervejas/pesquisaCervejas");
 		mv.addObject("estilos", estilos.findAll());
 		mv.addObject("sabores", Sabor.values());
 		mv.addObject("origens", Origem.values());	
 		
-		mv.addObject("cervejas",cervejas.filtrar(cervejaFilter, pageable));	
+		PageWrapper<Cerveja> paginalWrapper = new PageWrapper<>(cervejas.filtrar(cervejaFilter, pageable), httpServletRequest);
+		
+		mv.addObject("pagina",paginalWrapper);	
 		
 		return mv;
 		
--- src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -8,8 +8,14 @@ import javax.persistence.PersistenceContext;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -23,21 +29,33 @@ public class CervejasImpl implements CervejasQueries {
 
 	@Override
 	@Transactional(readOnly = true)
-	public List<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable) {
+	public Page<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable) {
 		
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
 		
-		//Calculo da paginação 2* a quantidade de registro
 		
 		int paginaAtual = pageable.getPageNumber();		
 		int totalRegistrosPorPagina = pageable.getPageSize();		
 		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
 		
-		//////////////////////////////		
-		
 		criteria.setFirstResult(primeiroRegistro);
 		criteria.setMaxResults(totalRegistrosPorPagina);
+		
+		Sort sort = pageable.getSort();
+		
+		if(sort !=null) {
+			
+			Sort.Order order = sort.iterator().next();
+			String field = order.getProperty();
+			criteria.addOrder(order.isAscending() ? Order.asc(field): Order.desc(field));
+		}
 
+		adicionalFiltro(filtro, criteria);
+		
+		return new PageImpl<Cerveja>(criteria.list(), pageable, total(filtro));
+	}
+
+	private void adicionalFiltro(CervejaFilter filtro, Criteria criteria) {
 		if(filtro!=null) {
 			if(!StringUtils.isEmpty(filtro.getSku())) {
 				criteria.add(Restrictions.eq("sku", filtro.getSku()));
@@ -68,10 +86,15 @@ public class CervejasImpl implements CervejasQueries {
 				criteria.add(Restrictions.le("sabor", filtro.getValorAte()));//menor
 			}			
 		}
-		
-		return criteria.list();
 	}
 	
+	private Long total(CervejaFilter filtro) {
+		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
+		adicionalFiltro(filtro, criteria);
+		criteria.setProjection(Projections.rowCount());
+		return  (long) criteria.uniqueResult();
+	}
+
 	private boolean isEstiloPresente(CervejaFilter filtro) {
 		return filtro.getEstilo() != null && filtro.getEstilo().getCodigo() != null;
 	}	
