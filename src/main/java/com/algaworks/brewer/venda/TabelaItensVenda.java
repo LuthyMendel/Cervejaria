e00a15a307c8e6b0447e4c508990fe26a879ef21
LuthyMendel <giom.01@hotmail.com>
2020-09-20 10:42:21 -0300

    Itens Vendas/testeUnitario

---
+++ src/main/java/com/algaworks/brewer/venda/TabelaItensVenda.java
@@ -0,0 +1,35 @@
+package com.algaworks.brewer.venda;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.ItemVenda;
+
+public class TabelaItensVenda {
+	
+	private List<ItemVenda> itens= new ArrayList<ItemVenda>();
+	
+	
+	//soma todos os valores
+	public BigDecimal getValorTotal() {
+		
+		return itens.stream()
+				.map(ItemVenda :: getValorTotal)
+				.reduce(BigDecimal:: add)
+				.orElse(BigDecimal.ZERO);
+	}
+	
+	
+	public void adicoinarItem(Cerveja cerveja, Integer quantidade) {
+		
+		ItemVenda itemVenda = new ItemVenda();
+		itemVenda.setCerveja(cerveja);
+		itemVenda.setQuantidade(quantidade);
+		itemVenda.setValorUnitario(cerveja.getValor());
+		
+		itens.add(itemVenda);
+	}
+
+}
